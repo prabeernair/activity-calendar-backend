9be3b40bbca620446cd782e487c8ed587d709c56
Oleksii Markhovskyi <olexiy.markhovsky@gmail.com>
2020-03-25 22:35:50 +0200

    add the 'activities' table to the DB

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,15 +1,31 @@
--- DROP DATABASE activity_calendar;
+-- @see: https://www.postgresql.org/docs/12/datatype.html
+
+-- DROP DATABASE IF EXISTS activity_calendar;
 CREATE DATABASE activity_calendar;
 \connect activity_calendar;
 
--- DROP TABLE users;
+-- DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id                 SERIAL         PRIMARY KEY,
-  name               VARCHAR(255),
+  name               VARCHAR(255)   ,
   email              VARCHAR(255)   UNIQUE NOT NULL,
-  magic_link         CHAR(32)       UNIQUE NOT NULL,
+  magic_link         UUID           UNIQUE NOT NULL,
+  last_logged_in_at  TIMESTAMP      NOT NULL,
   created_at         TIMESTAMP      NOT NULL,
-  last_logged_in_at  TIMESTAMP      NOT NULL
+  updated_at         TIMESTAMP      NOT NULL
 );
 
--- @todo: CREATE TABLE activities
+-- DROP TABLE IF EXISTS activities;
+CREATE TABLE activities (
+  id                 SERIAL         PRIMARY KEY,
+  user_id            INTEGER        NOT NULL,
+  type               VARCHAR(255)   NOT NULL,
+  fields             JSONB          , -- arbitrary properties (e.g. "distance" or "surface"), relevant for specific types
+  started_at         TIMESTAMP      NOT NULL,
+  duration           TSRANGE        NOT NULL,
+  created_at         TIMESTAMP      NOT NULL,
+  updated_at         TIMESTAMP      NOT NULL,
+  CONSTRAINT activities_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES users (id) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE CASCADE
+);
