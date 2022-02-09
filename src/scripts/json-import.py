5b33f3423c8ab6afb2d72c1507d037eef627d4ac
Oleksii Markhovskyi <olexiy.markhovsky@gmail.com>
2022-02-09 21:01:29 +0100

    put all remaining JSON fields into the 'fields: JSONB' insertable

--- src/scripts/json-import.py
+++ src/scripts/json-import.py
@@ -2,7 +2,6 @@ import argparse
 import sys
 import json
 from datetime import datetime, timedelta
-from typing import List
 from sqlalchemy import create_engine, exc, Table, MetaData
 from sqlalchemy.sql import select
 
@@ -22,23 +21,27 @@ parser.add_argument('file', type=open,
 args = parser.parse_args()
 
 
-def create_db_record(json_record: dict) -> dict:
-    db_record = dict(user_id=args.user_id,
+def create_db_record(user_id: int, json_record: dict[str, str | float | list]) -> dict[str, str | int | dict]:
+    """
+    Converts a JSON record to a DB-insertable dict:
+    `{user_id: int, type: str, duration: str, fields: dict}`.
+
+    >>> create_db_record(1, dict(type='run',
+    ...                          date='2022-02-09 12:06',
+    ...                          distance=5.3,
+    ...                          duration='0:25:29'))
+    {'user_id': 1, 'type': 'run', 'duration': '[2022-02-09 12:06:00, 2022-02-09 12:31:29]', 'fields': {'distance': 5.3}}
+    """
+
+    db_record = dict(user_id=user_id,
                      type=json_record['type'],
                      duration=get_ts_range(json_record['date'],
                                            json_record['duration'] if 'duration' in json_record else '0:00:00'))
 
-    # @todo: take all fields from "json_record" except ".type", ".date" and ".duration"
-    field_name_list = [
-        'distance',
-        'surface',
-        'averagePace',
-        'fastestSplitPace',
-        'slowestSplitPace',
-    ]
-    fields = dict({field_name: json_record[field_name]
-                   for field_name in field_name_list
-                   if field_name in json_record})
+    field_name_to_exclude_list = ['type', 'date', 'duration']
+    fields = {field_name: json_record[field_name]
+              for field_name in json_record
+              if field_name not in field_name_to_exclude_list}
 
     db_record['fields'] = fields if len(fields) else None
 
@@ -63,7 +66,7 @@ def get_ts_range(date_str: str, duration_str: str) -> str:
 
 if __name__ == '__main__':
     try:
-        json_record_list: List[dict] = json.load(args.file)
+        json_record_list: list[dict[str, str | float | list]] = json.load(args.file)
     except json.JSONDecodeError as error:
         sys.exit(f"File content can not be deserialized to JSON:\n{error}")
 
@@ -71,20 +74,20 @@ if __name__ == '__main__':
 
     try:
         with engine.connect() as connection:
+            user_id = int(args.user_id)
             users = Table('users', MetaData(), autoload=True, autoload_with=engine)
-            result = connection.execute(select([users]).where(users.c.id == args.user_id))
+            result = connection.execute(select([users]).where(users.c.id == user_id))
             row = result.fetchone()
 
             if row is None:
-                sys.exit(f"user with ID {args.user_id} is not found in DB")
+                sys.exit(f"user with ID {user_id} is not found in DB")
 
             activities = Table('activities', MetaData(), autoload=True, autoload_with=engine)
-            result = connection.execute(activities.insert(), [create_db_record(json_record)
+            result = connection.execute(activities.insert(), [create_db_record(user_id, json_record)
                                                               for json_record in json_record_list])
             print(f"inserted {result.rowcount} records into 'activities' table")
     except exc.InterfaceError as error:
-        sys.exit(("SQLAlchemy InterfaceError (check if PostgreSQL is running and the connection string is correct):\n"
-                  f"{error}"))
+        sys.exit(f"SQLAlchemy InterfaceError:\n{error}")
 
     import doctest
     doctest.testmod()
